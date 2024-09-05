3783da8537ef7987c17dd5982e94e2812f9ba070
HAE2NI <hae0_02ni@ewhain.net>
2024-09-05 23:18:24 +0900

    🚚 rename: 함수이름변경

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { headerInsertDate } from "./utils/header.js";
-import { onClickNewTaskBtn } from "./utils/makeNewTask.js";
+import { newTaskFn } from "./utils/makeNewTask.js";
 
-onClickNewTaskBtn();
+newTaskFn();
 headerInsertDate();
