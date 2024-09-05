e6742d2e25b438bedcfcf5f23449ef932ff01066
HAE2NI <hae0_02ni@ewhain.net>
2024-09-05 23:17:25 +0900

    ✨ feat: 상수형 dom 추가

--- js/constants/document.js
+++ js/constants/document.js
@@ -5,6 +5,7 @@ export const headerToday = document.getElementById("todayDateId");
 
 //할일 dom
 export const todolistWrapper = document.querySelector(".todoListWrapper");
+export const todosWrapper = document.querySelector(".todosWrapper");
 
 //할일 추가 button
 export const newTaskBtn = document.querySelector(".newTaskBtn");
