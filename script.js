7002c7e4e7f4492dd026f9b257681bdd59ec456f
hiwon-lee <leather2790@naver.com>
2024-09-07 19:07:14 +0900

    feat : localStorage에 저장, 삭제

--- script.js
+++ script.js
@@ -22,41 +22,57 @@ let dateElement = document.getElementById('date');
 
 dateElement.textContent = `${month}월 ${day}일 ${getDay}`;
 
-// 요소 추가하기
+// localStorage에 담길 투두항목
+let toDos = JSON.parse(localStorage.getItem('item')) || [];
+
+// id 생성기
+const generateID = () => Math.random().toString(36).substring(2, 9);
+
+// localStorage에 저장
+const saveToDos = () => {
+  localStorage.setItem('item', JSON.stringify(toDos));
+};
+
+// 투두리스트에 추가하기
 document
   .getElementById('todoContainer')
   .addEventListener('submit', function (event) {
     event.preventDefault();
     const inputField = document.getElementById('inputValue');
-    const inputValue = inputField.value;
+    const toDo = inputField.value;
 
-    if (inputValue) {
-      createToDoItem(inputValue);
+    if (toDo) {
+      const id = generateID();
+      const newToDo = { id: id, toDo: toDo };
+      toDos.push(newToDo);
+      saveToDos();
+      createToDoItem(toDo, id);
       inputField.value = ''; // 입력 필드 초기화
     }
   });
 
-var createToDoItem = (value) => {
+// 투두 요소 만들기
+var createToDoItem = (toDo, id) => {
   const newToDo = document.createElement('li');
   const toDoTitle = document.createElement('div');
-  toDoTitle.textContent = value;
+  toDoTitle.textContent = toDo;
   toDoTitle.classList.add('toDo');
   newToDo.classList.add('toDoItem');
 
   newToDo.appendChild(createIsCompleteButton());
   newToDo.appendChild(toDoTitle);
-  newToDo.appendChild(createDeleteButton(newToDo));
+  newToDo.appendChild(createDeleteButton(newToDo, id));
 
   toDoList.appendChild(newToDo);
 };
 
+// 투두 완료 버튼 생성
 const createIsCompleteButton = () => {
   const icon = document.createElement('span');
   icon.classList.add('material-icons');
   icon.textContent = 'radio_button_unchecked';
 
   icon.onclick = function () {
-    console.log('hello');
     if (icon.textContent === 'check_circled') {
       icon.textContent = 'radio_button_unchecked';
     } else {
@@ -64,18 +80,34 @@ const createIsCompleteButton = () => {
     }
   };
 
-  return icon; // 아이콘 반환
+  return icon;
 };
-const createDeleteButton = (item) => {
-  const deleteButton = document.createElement('div');
 
+// 투두 삭제 버튼 추가
+const createDeleteButton = (item, id) => {
+  const deleteButton = document.createElement('div');
+  console.log(id);
   deleteButton.textContent = '삭제';
   deleteButton.classList.add('mainButton');
   deleteButton.onclick = function () {
     toDoList.removeChild(item);
+    console.log(id);
+    toDos = toDos.filter((todo) => todo.id !== id);
+    console.log(toDos);
+    saveToDos();
   };
 
-  return deleteButton; // 버튼 반환
+  return deleteButton;
 };
 
-// 요소 삭제하기
+// 투두 리스트 렌더링
+const renderToDoList = () => {
+  toDoList.textContent = '';
+  toDos.forEach((item) => {
+    createToDoItem(item.toDo, item.id);
+  });
+};
+
+window.onload = function () {
+  renderToDoList();
+};
