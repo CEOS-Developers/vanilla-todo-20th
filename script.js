ab75547fb9ce1c1fe83429cd8bf2a4c417a5b52c
hiwon-lee <leather2790@naver.com>
2024-09-07 11:13:01 +0900

    feat : 날짜 추가하기

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //😍CEOS 20기 프론트엔드 파이팅😍
+const days = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+];
+const date = new Date();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const getDay = days[date.getDay()];
+
+let dateElement = document.getElementById('date');
+
+// dateElement.textContent = day;
+dateElement.textContent = `${month}월 ${day}일 ${getDay}`;
